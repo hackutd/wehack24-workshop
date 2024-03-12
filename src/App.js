3e8d6f4c164a3493dd47cc33d10d1777632c01f4
dasjoshua <jo3das@gmail.com>
2024-03-11 19:12:10 -0500

    starter code

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
 import React from "react";
 
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import Dashboard from "./components/Dashboard";
-
 
 function App() {
   return (
-    <div className="App">
-      <Navbar/>
-      <Hero/>
-      <Dashboard/>
-
-    </div>
+    <>
+    -- josh n reiki r sooooo cool --
+    </>
   );
 }
 
